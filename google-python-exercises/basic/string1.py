1e867827f6eafe1d88628a6852065474815c17eb
jstudenski <jstudenski@gmail.com>
2015-03-15 20:55:17 -0500

    string1 problems solved

--- google-python-exercises/basic/string1.py
+++ google-python-exercises/basic/string1.py
@@ -25,8 +25,10 @@
 # and donuts(23) returns 'Number of donuts: many'
 def donuts(count):
   # +++your code here+++
-  return
-
+  if count < 10:
+    return 'Number of donuts: %s' %count
+  else:
+    return 'Number of donuts: %s' %'many'
 
 # B. both_ends
 # Given a string s, return a string made of the first 2
@@ -35,7 +37,10 @@ def donuts(count):
 # is less than 2, return instead the empty string.
 def both_ends(s):
   # +++your code here+++
-  return
+  if len(s) < 2:
+    return ''
+  else:  
+    return s[:2] + s[-2:]
 
 
 # C. fix_start
@@ -48,9 +53,7 @@ def both_ends(s):
 # Hint: s.replace(stra, strb) returns a version of string s
 # where all instances of stra have been replaced by strb.
 def fix_start(s):
-  # +++your code here+++
-  return
-
+  return  s[0] + s[1:].replace(s[0], '*')
 
 # D. MixUp
 # Given strings a and b, return a single string with a and b separated
@@ -60,8 +63,8 @@ def fix_start(s):
 #   'dog', 'dinner' -> 'dig donner'
 # Assume a and b are length 2 or more.
 def mix_up(a, b):
-  # +++your code here+++
-  return
+
+  return b[:2] + a[2:] + ' ' + a[:2] + b[2:]
 
 
 # Provided simple test() function used in main() to print
